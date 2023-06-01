65112fddafd564b71abdaf0196306adeafcd0d34
Your Name <you@example.com>
2023-06-01 16:02:56 +0200

    Profik über Options und CSS stylen, Teil 2

--- main.js
+++ main.js
@@ -62,7 +62,9 @@ L.control.scale({
 let controlElevation = L.control.elevation({
     time: false,
     elevationDiv: "#profile",
-    height: 300, 
+    height: 300,
+    theme: "bike-tirol"
+    
 
 }).addTo(map);
 controlElevation.load("data/etappe10.gpx");
